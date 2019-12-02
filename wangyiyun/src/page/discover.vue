5c0094dcbf768f8b3aae0418f81b3e92a6b0f36a
Ehshujun <1140081019@qq.com>
2019-12-02 13:57:32 +0800

    hsj

--- wangyiyun/src/page/discover.vue
+++ wangyiyun/src/page/discover.vue
@@ -1,136 +1,316 @@
 <template>
   <div>
     <toubu></toubu>
-
-      <div class="recommend" v-if="fatherComponent='recommend'">
-        <div class="recommend-swiper">
-            <div class="swiper-container swiper-container1">
-                <div class="swiper-wrapper swiper-wrapper1">
-                    <div class="swiper-slide" style="background-image:url(http://p1.music.126.net/ERrZy5EpcwJnyZaWtEs0xw==/109951164501993643.jpg?imageView&quality=89)"></div>
-                    <div class="swiper-slide" style="background-image:url(http://p1.music.126.net/BY1gL1RGAVSHxP6-9d3Mqw==/109951164501999516.jpg?imageView&quality=89)"></div>
-                    <div class="swiper-slide" style="background-image:url(http://p1.music.126.net/M0yC8YnuC88NsqZEFR6FSg==/109951164503615246.jpg?imageView&quality=89)"></div>
-                    <div class="swiper-slide" style="background-image:url(http://p1.music.126.net/fXEQ1aYu3xmEpZSwu6u27A==/109951164503758739.jpg?imageView&quality=89)"></div>
-                    <div class="swiper-slide" style="background-image:url(http://p1.music.126.net/3UgrvzfeveXB7TkQluKJ_g==/109951164503770506.jpg?imageView&quality=89)"></div>
-                    <div class="swiper-slide" style="background-image:url(http://p1.music.126.net/3UgrvzfeveXB7TkQluKJ_g==/109951164503770506.jpg?imageView&quality=89)"></div>
-                    <div class="swiper-slide" style="background-image:url(http://p1.music.126.net/558UH6GafPpJIhtt_veWyw==/109951164502890194.jpg?imageView&quality=89)"></div>
-                    <div class="swiper-slide" style="background-image:url(http://p1.music.126.net/yI5Bzm8MOx2aDboAxmVn7w==/109951164502009550.jpg?imageView&quality=89)"></div>
-                    <div class="swiper-slide" style="background-image:url(http://p1.music.126.net/4AQ-neZ5OYjQ331CkA_I5g==/109951164503082619.jpg?imageView&quality=89)"></div>
+    <div class="discover">
+        <div class="recommend" v-if="fatherComponent='recommend'">
+            <div class="recommend-swiper">
+                <div class="swiper-container swiper-container1">
+                    <div class="swiper-wrapper swiper-wrapper1">
+                        <div class="swiper-slide" :style="{backgroundImage: 'url(' + lun[index].image + ')' }" v-for="(item,index) in lun" v-bind:key="index">
+                            <div class="swiper-slide-aaa">
+                                <img :src=lun[index].img alt="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="swiper-pagination swiper-pagination-white"></div>   
                 </div>
-                <!-- Add Pagination -->
-                <div class="swiper-pagination swiper-pagination-white"></div>
+                <!-- Add Arrows -->
+                <div class="swiper-button-next swiper-button-white swiper-button-tui1"></div>
+                <div class="swiper-button-prev swiper-button-white swiper-button-tui2"></div>
+                <div class="recommend-download">
+                    <a href="" class="recommend-btn btn"></a>
+                    <p>PC 安卓 iPhone WP iPad Mac 六大客户端</p>
+                </div>
             </div>
-            <!-- Add Arrows -->
-            <div class="swiper-button-next swiper-button-white swiper-button-tui1"></div>
-            <div class="swiper-button-prev swiper-button-white swiper-button-tui2"></div>
-            <div class="recommend-download">
-                <a href="" class="recommend-btn btn"></a>
-                <p>PC 安卓 iPhone WP iPad Mac 六大客户端</p>
+        </div>
+        <div class="recommend-white">
+            <div class="white-left">
+                <!-- 热门推荐 -->
+                <div class="recommend-white-re">
+                    <div class="recommend-white-re-top">
+                        <a href="/discover/playllist/" >{{tuijian}}</a>
+                        <div class="recommend-white-re-tab">
+                            <a href="/discover/playllist/" v-for="(item,index) in tab" v-bind:key="index">{{item}}</a>
+                        </div>
+                        <span class="recommend-white-re-span">
+                            <a href="/discover/playllist/">更多</a>
+                            <i></i>
+                        </span>
+                    </div>
+                    <ul class="recommend-white-re-bottom">
+                        <li v-for="(item,index) in list" v-bind:key="index">
+                            <div class="recommend-white-re-cover">
+                                <img v-bind:src="item.img" alt="">
+                                <div class="recommend-white-cover-bottom">
+                                    <a href="javascript:;" class="recommend-white-re-play"></a>
+                                    <span class="recommend-white-re-music"></span>
+                                    <span class="recommend-white-re-nb">{{item.listen}}</span>
+                                </div>
+                            </div>
+                            <p><a href=""><i></i>{{item.title}}</a></p>
+                        </li>
+                    </ul>
+                </div>
+                <!-- 个性化推荐 -->
+                <div class="recommend-white-re">
+                    <div class="recommend-white-re-top">
+                        <a href="/discover/playllist/" >{{tuijian1}}</a>
+                    </div>
+                    <ul class="recommend-white-re-bottom">
+                        <li>
+                            <div class="recommend-white-re-cover">
+                                <span class="recommend-white-re-cover1">星期一</span>
+                                <span class="recommend-white-re-cover2">25</span>
+                                <span class="recommend-white-re-cover3" href="/discover/recommend/taste"></span>
+                            </div>
+                            <p><a href=""><i></i>每日歌曲推荐</a></p>
+                            <p class="recommend-white-re-bottom-p">根据你的口味生成，<br>每天6:00更新</p>
+                        </li>
+                        <li v-for="(item,index) in list1" v-bind:key="index">
+                            <div class="recommend-white-re-cover">
+                                <img :src=item.img alt="">
+                                <div class="recommend-white-cover-bottom">
+                                    <a href="javascript:;" class="recommend-white-re-play"></a>
+                                    <span class="recommend-white-re-music"></span>
+                                    <span class="recommend-white-re-nb">{{item.listen}}</span>
+                                </div>
+                            </div>
+                            <p><a href=""><i></i>{{item.title}}</a></p>
+                            <p class="recommend-white-re-bottom-p">{{item.title1}}</p>
+                        </li>
+                        
+                    </ul>
+                </div>
+                <!-- 新碟上架 -->
+                <div class="recommend-white-re recommend-white-die">
+                    <div class="recommend-white-re-top">
+                        <a href="/discover/playllist/" >{{tuijian2}}</a>
+                        <span class="recommend-white-re-span">
+                            <a href="/discover/playllist/">更多</a>
+                            <i></i>
+                        </span>
+                    </div>
+                    <div class="recommend-white-disk">
+                        <div class="recommend-white-disk-inner">
+                            <div class="swiper-container2">
+                                <div class="swiper-wrapper ">
+                                    <div class="swiper-slide" v-for="(item,index) in list2" v-bind:key="index">
+                                        <div class="recommend-white-disk-img">
+                                            <img :src=item.img alt="">
+                                            <a class="recommend-white-disk-msk" href="/album?id=83487317"></a>
+                                            <a class="recommend-white-disk-play" href="javascript:;"></a>
+                                        </div>
+                                        <p><a href="/album?id=83487317">{{item.text1}}</a></p>
+                                        <p><a href="/album?id=83487317">{{item.text2}}</a></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-button-next swiper-button-next1 swiper-button-black"></div>
+                        <div class="swiper-button-prev swiper-button-prev1 swiper-button-black"></div>
+                    </div>         
+                </div>
+                <!-- 榜单 -->
+                <div class="recommend-white-re recommend-white-die">
+                    <div class="recommend-white-re-top">
+                        <a href="/discover/playllist/" >{{tuijian3}}</a>
+                        <span class="recommend-white-re-span">
+                            <a href="/discover/playllist/"><text>更多</text>更多</a>
+                            <i></i>
+                        </span>
+                    </div>
+                    <div class="recommend-white-flag">
+                        <div class="recommend-white-blk" v-for="(item,index) in yun" v-bind:key="index">
+                            <div class="recommend-white-blk-top">
+                                <div class="recommend-white-blk-top1">
+                                    <img :src=yun[index].img alt="">
+                                    <a class="recommend-white-blk-top11" href=""></a>
+                                </div>
+                                <div class="recommend-white-blk-top2">
+                                    <a class="recommend-white-blk-top21" href=""><p>{{yun[index].music}}</p></a>
+                                    <a href="">播放</a>
+                                    <a href="">收藏</a>
+                                </div>
+                            </div>
+                            <div class="recommend-white-blk-bottom">
+                                <ul >
+                                    <li v-for="(item,index) in yun[index].list" v-bind:key="index">
+                                        <span>{{index+1}}</span>
+                                        <a href="">{{item}}</a>
+                                    </li>
+                                    <div class="more">
+                                        <a href="/discover/toplist?id=19723756">查看全部 ></a>
+                                    </div>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- 侧边 -->
+            <div class="white-right">
+                <div class="white-right-myinfo">
+                    <div>
+                        <a href="/user/home?id=1394991253" class="lin">
+                            <img src="http://p1.music.126.net/cYvwvZF8kr1B1AX2jZeo_g==/109951163207976673.jpg?param=80y80" alt="">
+                        </a>
+                        <div class="right">
+                            <h4><a href="/user/home?id=1394991253">吃肉不吧唧吧唧嘴</a></h4>
+                            <p>
+                                <a href="/user/level" class="level">
+                                    7<i></i> 
+                                </a>
+                            </p>
+                            <div class="button">
+                                <a href="javascript:;"><i>签到</i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <ul>
+                        <li class="fst">
+                            <a href="/user/event?id=1394991253">
+                                <strong>0</strong>
+                                <span>动态</span>
+                            </a>
+                        </li>
+                        <li class="fst">
+                            <a href="/user/event?id=1394991253">
+                                <strong>4</strong>
+                                <span>关注</span>
+                            </a>
+                        </li>
+                        <li class="fst">
+                            <a href="/user/event?id=1394991253">
+                                <strong>0</strong>
+                                <span>粉丝</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="white-right-singer">
+                    <h3>
+                        <span>{{sing.title}}</span>
+                        <a href="/discover/artist/signed/" class="more">{{sing.all}} ></a>
+                    </h3>
+                    <ul>
+                        <li v-for="(item,index) in sing.list" v-bind:key="index">
+                            <a href="/user/home?id=29879272">
+                                <div class="head"><img :src=item.img alt=""></div>
+                                <div class="ifo">
+                                    <h4>{{item.singer}}</h4>
+                                    <p>{{item.message}}</p>
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="white-right-singers white-right-singer">
+                    <h3>
+                        <span>{{sings.title}}</span>
+                    </h3>
+                    <ul>
+                        <li v-for="(item,index) in sings.list" v-bind:key="index">
+                            <a href="/user/home?id=278438485">
+                                <img :src=item.img alt="">                               
+                            </a>
+                            <div class="ifo">
+                                <h4>
+                                    <a href="">{{item.singer}}</a>
+                                    <i class="u-icn-1"></i>
+                                </h4>
+                                <p>{{item.message}}</p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
 
     <foot></foot>
+    
   </div>
 </template>
 <script>
 import toubu from "../page/head";
 import foot from "../page/foot";
 import Swiper from 'swiper'
-
+import axios from '../../node_modules/axios'
+import $ from 'jquery'
 export default {
-  props: ["fatherComponent"],
+    data() {
+        return {
+            current:"0",
+            tuijian:"",
+            tab:[],
+            list:[],
+            tuijian1:"",
+            list1:[],
+            tuijian2:"",
+            list2:[],
+            yun:"",
+            tuijian3:"",
+            sing:"",
+            sings:"",
+            lun:""
+        }
+    },
+    methods: {
+        
+    },
     mounted() {
-        var swiper1=new Swiper('.swiper-container', {
-            loop:true,
+        this.$http.get("../../static/json/hotdoor.json").then((res)=>{
+            // console.log(res.data[6].list)
+            this.tuijian=res.data[0].tuijian
+            this.tab=res.data[0].tab
+            this.list=res.data[0].list
+            this.tuijian1=res.data[1].tuijian
+            this.list1=res.data[1].list
+            this.tuijian2=res.data[2].tuijian
+            this.list2=res.data[2].list
+            this.yun=res.data[3].yun1
+            this.tuijian3=res.data[3].tuijian
+            this.sing=res.data[4]
+            this.sings=res.data[5]
+            this.lun=res.data[6].list
+        });
+        
+        var swiper1=new Swiper('.swiper-container1', {
+            loop : true,
             spaceBetween: 30,
             effect: 'fade',
+            autoplay: {
+                delay: 3000,
+            },
             pagination: {
                 el: '.swiper-pagination',
                 clickable: true,
             },
             navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
+                nextEl: '.swiper-button-tui1',
+                prevEl: '.swiper-button-tui2',
             },
         });
+        var swiper2 = new Swiper('.swiper-container2', {
+            loop : true,
+            slidesPerView: 5,
+            slidesPerGroup : 5,
+            // direction: getDirection(),
+            navigation: {
+                nextEl: '.swiper-button-next1',
+                prevEl: '.swiper-button-prev1',
+            },
+            
+        });
+
     },
-  components: { toubu, foot }
+    
+    components: { toubu, foot }
 };
 </script>
 
-
-
-
 <style scope>
 @import "../../node_modules/swiper/css/swiper.min.css";
-.head_02 {
-  display: block;
-}
-*{
-    margin: 0;padding: 0;
-}
-.recommend{
-    width: 100%;
-    height: 285px;
-    background: black;
-}
-.recommend-swiper {
-    margin: 0 auto;
-    width: 985px;
-    height: 285px;
-    position: relative;
-}
-.swiper-container1{
-    float: left;
-    width: 730px;
-    height: 285px;
-    position: relative;
-    overflow: hidden;
-    list-style: none;
-    padding: 0;
-    z-index: 1;
-}
-.swiper-slide {
-    background-position: center;
-    background-size: cover;
-}
-.swiper-pagination-bullet{
-    background: white;
-}
-.swiper-pagination-bullet-active{
-    background:#9f0606;
-}
-.swiper-button-tui1{
-    right: -60px !important;
-}
-.swiper-button-tui2{
-    left: -60px !important;
-}
-.recommend-download{
-    position: absolute;
-    top: 0;
-    z-index: 20;
-    right: -1px;
-    width: 254px;
-    height: 285px;
-    background: url('https://s2.music.126.net/style/web2/img/index/download.jpg?965a94c87533ccbde73106bea32344d9') no-repeat 0 0;
-}
-.recommend-download .recommend-btn{
-    display: block;
-    width: 215px;
-    height: 56px;
-    margin: 186px 0 0 19px;
-    background: url('https://s2.music.126.net/style/web2/img/index/download.jpg?965a94c87533ccbde73106bea32344d9') no-repeat;
-    background-position: 0 9999px;
-}
-.recommend-download .recommend-btn:hover{
-    background: url('https://s2.music.126.net/style/web2/img/index/download.jpg?965a94c87533ccbde73106bea32344d9') no-repeat;
-    background-position: 0px -290px;
-}
-.recommend-download>p{
-    font-size: 12px;
-    margin: 10px auto;
-    text-align: center;
-    color: #8d8d8d;
-}
+@import "../../static/css/discover.css";
+
 </style>
